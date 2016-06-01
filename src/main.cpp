8647fa323f9581a2d10ed02bff54aee798acf938
Dominik Schildknecht <domischi@student.ethz.ch>
2016-06-01 07:56:21 +0200

    Implemented with parapack

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,6 @@
-//#include <alps/osiris/comm.h>
 
-#include "single-spin-flip/ssf.h"
-
-#include <iostream>
+#include <alps/parapack/parapack.h>
 
 int main(int argc,char** argv){
-    //if (argc!=2){
-    //    std::cerr<< "Usage: mc++ input_file.xml"<<std::endl;
-    //    return 1;
-    //}
-    //else{
-    // TODO this needs to be generalized for any potential factory.... 
-        return alps::scheduler::start(argc,argv, ssf_factory());
-    //}
+    return alps::parapack::start(argc,argv);
 }
---
+++ src/parapack_register.cpp
@@ -0,0 +1,7 @@
+#include "single-spin-flip/ssf.h"
+
+PARAPACK_SET_VERSION("my version");
+PARAPACK_SET_COPYRIGHT("my copyright");
+PARAPACK_REGISTER_WORKER(ssf_worker, "ssf");
+PARAPACK_REGISTER_EVALUATOR(ssf_evaluator, "ssf");
+
