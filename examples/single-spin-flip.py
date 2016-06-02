4bd8cde31b6f2c527b34d0c914e35cb1e395f11f
Dominik Schildknecht <domischi@student.ethz.ch>
2016-06-02 07:45:07 +0200

    updated the example with single-spin-flip

--- examples/single-spin-flip.py
+++ examples/single-spin-flip.py
@@ -1,27 +1,8 @@
 # ****************************************************************************
 # 
-# ALPS Project: Algorithms and Libraries for Physics Simulations
-# 
-# ALPS Libraries
-# 
-# Copyright (C) 2009-2010 by Brigitte Surer <surerb@phys.ethz.ch> 
-# 
-# This software is part of the ALPS libraries, published under the ALPS
-# Library License; you can use, redistribute it and/or modify it under
-# the terms of the license, either version 1 or (at your option) any later
-# version.
-#  
-# You should have received a copy of the ALPS Library License along with
-# the ALPS Libraries; see the file LICENSE.txt. If not, the license is also
-# available from http://alps.comp-phys.org/.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-# FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT 
-# SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE 
-# FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE, 
-# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-# DEALINGS IN THE SOFTWARE.
+# This tutorial shows how to simply generate the input files, start the task in python (not recommended on a cluster) and then evaluate. It is designed to the dipolar XY system in 2D, where the order parameter is a staggered magnetic field (stripe like)
+#
+# To a big proportion this tutorial is written by Brigitte Surer <surerb@phys.ethz.ch> in the spinmc part of the ALPS library. As it is similar the code was reused. 
 # 
 # ****************************************************************************
 
@@ -29,66 +10,58 @@ import pyalps
 import matplotlib.pyplot as plt
 import pyalps.plot
 from numpy import linspace
+
 #prepare the input parameters
 parms = []
 for l in [4,8,16]: 
     for t in linspace(0,2.5,25):
         parms.append(
             { 
-              'LATTICE'        : "square lattice", 
-              'T'              : t,
-              'J'              : 1 ,
-              'THERMALIZATION' : 10000,
-              'SWEEPS'         : 4000000,
-              'UPDATE'         : "ssf",
-              'L'              : l
-            }
-        )
+                 'LATTICE'        : "square lattice", 
+                 'T'              : t,
+                 'D'              : 1 ,
+                 'THERMALIZATION' : 10000,
+                 'SWEEPS'         : 4000000,
+                 'UPDATE'         : "ssf",
+                 'cutoff_distance': 3.,
+                 'L'              : l
+               }
+           )
+
 #write the input file and run the simulation
 input_file = pyalps.writeInputFiles('parm',parms)
 pyalps.runApplication('mc++',input_file,Tmin=5)
 # use the following instead if you have MPI
 #pyalps.runApplication('mc++',input_file,Tmin=5,MPI=2)
 
-pyalps.evaluateSpinMC(pyalps.getResultFiles(prefix='parm'))
+#load the susceptibility and collect it as function of temperature T
+data = pyalps.loadMeasurements(pyalps.getResultFiles(prefix='parm'),['M staggered', 'c_V', 'BinderCumulant staggered', 'susceptibility staggered'])
+M = pyalps.collectXY(data,x='T',y='M staggered',foreach=['L'])
+chi = pyalps.collectXY(data,x='T',y='susceptibility staggered',foreach=['L'])
+c_v= pyalps.collectXY(data,x='T',y='c_V',foreach=['L'])
+binder = pyalps.collectXY(data,x='T',y='BinderCumulant staggered',foreach=['L'])
 
-##load the susceptibility and collect it as function of temperature T
-#data = pyalps.loadMeasurements(pyalps.getResultFiles(prefix='parm7a'),['|Magnetization|', 'Connected Susceptibility', 'Specific Heat', 'Binder Cumulant', 'Binder Cumulant U2'])
-#magnetization_abs = pyalps.collectXY(data,x='T',y='|Magnetization|',foreach=['L'])
-#connected_susc = pyalps.collectXY(data,x='T',y='Connected Susceptibility',foreach=['L'])
-#spec_heat = pyalps.collectXY(data,x='T',y='Specific Heat',foreach=['L'])
-#binder_u4 = pyalps.collectXY(data,x='T',y='Binder Cumulant',foreach=['L'])
-#binder_u2 = pyalps.collectXY(data,x='T',y='Binder Cumulant U2',foreach=['L'])
-#
-##make plots
-#plt.figure()
-#pyalps.plot.plot(magnetization_abs)
-#plt.xlabel('Temperature $T$')
-#plt.ylabel('Magnetization $|m|$')
-#plt.title('2D Ising model')
-#
-#plt.figure()
-#pyalps.plot.plot(connected_susc)
-#plt.xlabel('Temperature $T$')
-#plt.ylabel('Connected Susceptibility $\chi_c$')
-#plt.title('2D Ising model')
-#
-#plt.figure()
-#pyalps.plot.plot(spec_heat)
-#plt.xlabel('Temperature $T$')
-#plt.ylabel('Specific Heat $c_v$')
-#plt.title('2D Ising model')
-#
-#plt.figure()
-#pyalps.plot.plot(binder_u4)
-#plt.xlabel('Temperature $T$')
-#plt.ylabel('Binder Cumulant U4 $g$')
-#plt.title('2D Ising model')
-#
-#plt.figure()
-#pyalps.plot.plot(binder_u2)
-#plt.xlabel('Temperature $T$')
-#plt.ylabel('Binder Cumulant U2 $g$')
-#plt.title('2D Ising model')
-#plt.show()
+#make plots
+plt.figure()
+pyalps.plot.plot(M)
+plt.xlabel('Temperature $T$')
+plt.ylabel('staggered Magnetization $M$')
+plt.title('2D XY model with dipolar interaction')
 
+plt.figure()
+pyalps.plot.plot(chi)
+plt.xlabel('Temperature $T$')
+plt.ylabel('staggered Susceptibility $\chi$')
+plt.title('2D XY model with dipolar interaction')
+
+plt.figure()
+pyalps.plot.plot(c_v)
+plt.xlabel('Temperature $T$')
+plt.ylabel('Specific Heat $c_V$')
+plt.title('2D XY model with dipolar interaction')
+
+plt.figure()
+pyalps.plot.plot(binder)
+plt.xlabel('Temperature $T$')
+plt.ylabel('Binder Cumulant')
+plt.title('2D XY model with dipolar interaction')
