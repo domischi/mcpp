ec17ab27f5edb2ba10569f9e849890d398c0b749
Dominik Schildknecht <dominik.schildknecht@psi.ch>
2016-09-14 16:55:32 +0200

    now the git hash is saved in the out.xml file, such that there is full information how I gathered the data

--- src/parapack_register.cpp
+++ src/parapack_register.cpp
@@ -1,6 +1,7 @@
+#include "version/version.h"
 #include "models/xy.h"
 
-PARAPACK_SET_VERSION("");
+PARAPACK_SET_VERSION(GIT_COMMIT_HASH);
 PARAPACK_SET_COPYRIGHT("Using mc++\n  copyright (c) by Dominik Schildknecht <dominik.schildknecht@psi.ch>\n  if you reuse this project, please mention the ALPS project and me\n");
 PARAPACK_REGISTER_WORKER(xy_worker, "xy");
 PARAPACK_REGISTER_EVALUATOR(xy_evaluator, "xy");
