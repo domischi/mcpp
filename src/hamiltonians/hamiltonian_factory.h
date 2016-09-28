a9cd5b5df5ea70faa280cae352c4c12398ce2579
Dominik Schildknecht <domischi@student.ethz.ch>
2016-09-28 18:27:14 +0200

    split away the disordered dipolar interaction

--- src/hamiltonians/hamiltonian_factory.h
+++ src/hamiltonians/hamiltonian_factory.h
@@ -3,21 +3,30 @@
 
 #include <vector>
 #include <memory> //unique_ptr
-
+#include <iostream>
 #include "xy_dipolar.h"
+#include "xy_dipolar_disordered.h"
 #include "xy_exchange.h"
 #include "xy_shape_anisotropy.h"
 
 std::vector<std::shared_ptr<XY_Hamiltonian>> HamiltonianFactory(alps::Parameters const& params){
     std::vector<std::shared_ptr<XY_Hamiltonian>> HamiltonianList;
-    if(params.defined("D") && static_cast<double>(params["D"])!=0.){
-        HamiltonianList.push_back(std::make_shared<XY_Dipole>(params)); 
+     if(params.defined("D") && static_cast<double>(params["D"])!=0.){
+        if((params.defined("Dilution Rate") && static_cast<double>(params["Dilution Rate"])!=0.) || (params.defined("Position Disorder") && static_cast<double>(params["Position Disorder"])!=0.)){
+            HamiltonianList.push_back(std::make_shared<XY_Dipole_Disordered>(params));
+        }
+        else
+            HamiltonianList.push_back(std::make_shared<XY_Dipole>(params));
     }
     if(params.defined("J") && static_cast<double>(params["J"])!=0.){
         HamiltonianList.push_back(std::make_shared<XY_Exchange>(params)); 
     }
     if(params.defined("p_max") && static_cast<double>(params["A"])!=0.){
         HamiltonianList.push_back(std::make_shared<XY_Shape_Anisotropy>(params)); 
+    }
+    if(HamiltonianList.size()==0) {
+        std::cerr << "NO HAMILTONIAN LOADED";
+        std::exit(56);
     }
     return HamiltonianList;
 }
