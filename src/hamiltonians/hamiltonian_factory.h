eff7ccefa8f5048fcb001365c028b7b43afee985
Dominik Schildknecht <domischi@student.ethz.ch>
2016-09-28 18:54:07 +0200

    actually the disordered Hamiltonian can be implemented as a template...

--- src/hamiltonians/hamiltonian_factory.h
+++ src/hamiltonians/hamiltonian_factory.h
@@ -5,7 +5,6 @@
 #include <memory> //unique_ptr
 #include <iostream>
 #include "xy_dipolar.h"
-#include "xy_dipolar_disordered.h"
 #include "xy_exchange.h"
 #include "xy_shape_anisotropy.h"
 
@@ -13,10 +12,10 @@ std::vector<std::shared_ptr<XY_Hamiltonian>> HamiltonianFactory(alps::Parameters
     std::vector<std::shared_ptr<XY_Hamiltonian>> HamiltonianList;
      if(params.defined("D") && static_cast<double>(params["D"])!=0.){
         if((params.defined("Dilution Rate") && static_cast<double>(params["Dilution Rate"])!=0.) || (params.defined("Position Disorder") && static_cast<double>(params["Position Disorder"])!=0.)){
-            HamiltonianList.push_back(std::make_shared<XY_Dipole_Disordered>(params));
+            HamiltonianList.push_back(std::make_shared<XY_Dipole<true>>(params));
         }
         else
-            HamiltonianList.push_back(std::make_shared<XY_Dipole>(params));
+            HamiltonianList.push_back(std::make_shared<XY_Dipole<false>>(params));
     }
     if(params.defined("J") && static_cast<double>(params["J"])!=0.){
         HamiltonianList.push_back(std::make_shared<XY_Exchange>(params)); 
--- src/hamiltonians/xy_dipolar.h
+++ src/hamiltonians/xy_dipolar.h
@@ -10,6 +10,7 @@
 
 #include "xy_hamiltonian.h"
 
+template<bool DISORDERED>
 class XY_Dipole : public XY_Hamiltonian{
 public:
     XY_Dipole (alps::Parameters const& params) : 
@@ -17,9 +18,17 @@ public:
     D(params.value_or_default("D",1)),
     L(params["L"]),
     N(L*L),
-    gh(params){ 
+    gh(params) {
+        if(DISORDERED){
+            dilution_rate=params.value_or_default("Dilution Rate", 0.);
+            position_std_dev=static_cast<double>(params.value_or_default("Position Disorder", 0.))*static_cast<double>(params.value_or_default("a",1.));
+        }
+        else{
+            dilution_rate=0;
+            position_std_dev=0;
+        }
         cutoff_distance=static_cast<double>(params.value_or_default("cutoff_distance",3))*static_cast<double>(params.value_or_default("a",1.));
-        Init_Lookup_Tables();
+        Init_Lookup_Tables(params["DISORDER_SEED"]);
     } 
     virtual double SingleSiteEnergy(std::vector<double> const& spins, int i){
         double e=0.;
@@ -32,6 +41,8 @@ public:
 private:
     int L,N;
     double D;
+    double dilution_rate;
+    double position_std_dev;
     double cutoff_distance;
     alps::graph_helper<> gh;
     //Lookup tables 
@@ -42,23 +53,34 @@ private:
     typedef typename alps::graph_helper<>::vector_type vector_type;
     typedef typename alps::graph_helper<>::basis_vector_iterator basis_vector_iterator;
     typedef typename alps::graph_helper<>::site_iterator site_iterator;
-    
+
+    //This is a templated if, should be optimized away as it is constant in the class
     inline int reduced_index(int i, int j){
-        int xi,yi,xj,yj,x,y;
-        xi=i/L;
-        yi=i%L;
-        xj=j/L;
-        yj=j%L;
-        x=(xi-xj+L)%L;
-        y=(yi-yj+L)%L;
-        return L*x+y;
+        if(DISORDERED)
+            return i+N*j;
+        else {
+            int xi,yi,xj,yj,x,y;
+            xi=i/L;
+            yi=i%L;
+            xj=j/L;
+            yj=j%L;
+            x=(xi-xj+L)%L;
+            y=(yi-yj+L)%L;
+            return L*x+y;
+        }
     }
     inline int reduced_index(std::pair<int,int> p) {
         return reduced_index(p.first,p.second);
     }
-    void Init_Lookup_Tables(){
-        dist_3=std::vector<double>(N);
-        phi=std::vector<double>(N);
+    void Init_Lookup_Tables(int DISORDER_SEED){
+        if(DISORDERED) {
+            dist_3=std::vector<double>(N*N);
+            phi=std::vector<double>(N*N);
+        }
+        else {
+            dist_3=std::vector<double>(N);
+            phi=std::vector<double>(N);
+        }
         std::vector<vector_type> basis;
         basis_vector_iterator v, v_end;
         for(std::tie(v,v_end)=gh.basis_vectors();v!= v_end;++v){
@@ -83,12 +105,23 @@ private:
         }
         for(int i=0;i<gh.num_sites();++i) neighbour_list.push_back(std::vector<int>());
         std::map<int,double> dist_map;
+        std::mt19937 rng(DISORDER_SEED);
+        //Position disorder 
+        std::vector<double> dR(gh.dimension()*N);
+        if(DISORDERED&&position_std_dev>0.) { //should again be optimized away
+            std::normal_distribution<double> dist(0,position_std_dev);
+            for(int i=0;i<2*N;++i) dR[i]=dist(rng);
+        }
         for(site_iterator s_iter = gh.sites().first; s_iter !=gh.sites().second; ++s_iter )
         for(site_iterator s_iter2= gh.sites().first; s_iter2!=gh.sites().second; ++s_iter2)
         for(auto& p : periodic_translations)
             if(*s_iter!=*s_iter2){
                 vector_type c1(gh.coordinate(*s_iter));
                 vector_type c2(gh.coordinate(*s_iter2));
+                for(int d=0;d<gh.dimension();++d){
+                    c1[d]+=dR[*s_iter +d];
+                    c2[d]+=dR[*s_iter2+d];
+                }
                 double dist=distance(c1,c2,p);
                 int ri = reduced_index(*s_iter, *s_iter2);
                 if(dist<=cutoff_distance && (dist_map[ri]==0. || dist<=dist_map[ri])){ //new value is smaller than the previous calculated for this pair
@@ -98,6 +131,23 @@ private:
                     neighbour_list[*s_iter].push_back(*s_iter2);
                 }
             }
+        //Dilution Disorder, the test for DISORDER is actually not needed, nevertheless explains that this should be optimized away in the pure phase 
+        if(DISORDERED && dilution_rate > 0.){
+            std::cout << "WARNING: dilution is only implemented for the dipolar interaction!"<<std::endl;
+            std::vector<int> site_list(N);
+            iota(site_list.begin(),site_list.end(),0);// List of the sites
+            std::shuffle(site_list.begin(),site_list.end(),rng);
+            int Num_Diluted_Sites=static_cast<int>(std::round(dilution_rate*N));
+            std::cout << "Remove "<<Num_Diluted_Sites<<"/"<<N<<" resulting in an effective dilution rate of "<<std::setprecision(3)<<Num_Diluted_Sites*1.0/N<< " ~ "<<dilution_rate<<std::endl;
+            site_list.resize(Num_Diluted_Sites);
+            for (auto& l : site_list) {
+                neighbour_list[l]=std::vector<int>(); //make sure these sites have no neighbours 
+            }
+            for(auto& nl : neighbour_list){ //make sure that they are not neighbours of others
+                for(auto& s : site_list)
+                    nl.erase(std::remove(nl.begin(),nl.end(),s),nl.end());
+            }
+        }
         //Sort & Shrink the neighbour_list
         for(auto& nl : neighbour_list) {
             std::sort(nl.begin(),nl.end());
