edb4ad8a9e8e99d3dd61ed76a249371b9575239c
Dominik Schildknecht <domischi@student.ethz.ch>
2016-10-16 08:33:34 +0200

    changed the fss-sliders script to also do fss for cv

--- scripts/fss-sliders.py
+++ scripts/fss-sliders.py
@@ -9,35 +9,50 @@
 import os
 import pyalps
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider, Button, RadioButtons
+from matplotlib.widgets import Slider
 import pyalps.plot
-from numpy import linspace
+from numpy import linspace, log
+
+def f_alpha(Le, Lo, b=2, d=2):
+    return 2-d*log(b)/log(Le)
+def f_beta(Le, Lo, b=2, d=2):
+    return log(b)/log(Le)*(log(Lo)/log(b)-2)
+def f_gamma(Le, Lo, b=2, d=2):
+    return log(b)/log(Le)*(2*log(Lo)/log(b)-d)
+def f_nu(Le, Lo, b=2, d=2):
+    return log(b)/log(Le)
 
 #load the susceptibility and collect it as function of temperature T
 data = pyalps.loadMeasurements(pyalps.getResultFiles(prefix='parm'),['M staggered', 'c_V', 'BinderCumulant staggered', 'susceptibility staggered'])
-#M = pyalps.collectXY(data,x='T',y='M staggered',foreach=['L'])
-#chi = pyalps.collectXY(data,x='T',y='susceptibility staggered',foreach=['L'])
-#c_v= pyalps.collectXY(data,x='T',y='c_V',foreach=['L'])
-#binder = pyalps.collectXY(data,x='T',y='BinderCumulant staggered',foreach=['L'])
 
-Tc0=1.2
-Tc_min=1.
-Tc_max=1.5
-nu0=1.6
-nu_min=0.1
-nu_max=3
-gamma0= 7./4
-gamma_min=0.
-gamma_max=3
-beta0=0.18
-beta_min=0.1
-beta_max=0.3
+Tc0=0.82
+Tc_min=0.7
+Tc_max=0.9
+le0=1.78
+le_min=1.5
+le_max=2.0
+lo0=1.78
+lo_min=1.5
+lo_max=2.0
+alpha0=f_alpha(le0,lo0)
+beta0 =f_beta(le0,lo0)
+gamma0=f_gamma(le0,lo0)
+nu0   =f_nu(le0,lo0)
 
+left_start=0.05
+right_end=1-left_start
+bottom_start=0.3
+top_end=1
+boarder_width=0.1
+width=0.5-boarder_width
+height=0.35-boarder_width
 
-fig, axM = plt.subplots()
-plt.subplots_adjust(left=0.375, bottom=0.3, right = 0.625)
-axChi = plt.axes([0.05, 0.3, 0.2 ,0.6])
-axBinder = plt.axes([0.7, 0.3, 0.2, 0.6])
+fig = plt.subplots()
+plt.subplots_adjust(left=left_start, bottom=bottom_start, right = width-boarder_width, top=bottom_start+height-boarder_width)
+axM= plt.axes([left_start, bottom_start,width,height])
+axChi = plt.axes([left_start, bottom_start+height+boarder_width,width,height])
+axBinder = plt.axes([left_start+width+boarder_width, bottom_start,width,height])
+axCv = plt.axes([left_start+width+boarder_width,  bottom_start+height+boarder_width, width,height]) 
 plt.sca(axChi)
 chi = pyalps.collectXY(data,x='T',y='susceptibility staggered',foreach=['L'])
 for d in chi:
@@ -59,7 +74,6 @@ for d in binder:
     d.x = d.x/Tc0
     l = d.props['L']
     d.x = d.x * pow(float(l),1/nu0)
-    #d.y = d.y * pow(float(l),-gamma/nu)
 plt.cla()
 pyalps.plot.plot(binder)
 plt.xlabel(r'$(T-T_c)L^{\frac{1}{\nu}}$', fontsize='x-large')
@@ -79,23 +93,38 @@ pyalps.plot.plot(M)
 plt.xlabel(r'$(T-T_c)L^{\frac{1}{\nu}}$', fontsize='x-large')
 plt.ylabel(r'$ML^{\frac{\beta}{\nu}}$', fontsize='x-large')
 plt.title(r'$M$', fontsize='x-large')
+M = pyalps.collectXY(data,x='T',y='c_V',foreach=['L'])
+plt.sca(axCv)
+for d in M:
+    d.props['label']='L='+str(d.props['L'])
+    d.x -= Tc0
+    d.x = d.x/Tc0
+    l = d.props['L']
+    d.x = d.x * pow(float(l),1/nu0)
+    d.y = d.y * pow(float(l),-alpha0/nu0)
+plt.cla()
+pyalps.plot.plot(M)
+plt.xlabel(r'$(T-T_c)L^{\frac{1}{\nu}}$', fontsize='x-large')
+plt.ylabel(r'$C_VL^{\frac{-\alpha}{\nu}}$', fontsize='x-large')
+plt.title(r'$C_V$', fontsize='x-large')
 
 axcolor = 'lightgoldenrodyellow'
-axbeta = plt.axes([0.1, 0.05, 0.7, 0.03], axisbg=axcolor)
-axgamma = plt.axes([0.1, 0.1, 0.7, 0.03], axisbg=axcolor)
-axnu = plt.axes([0.1, 0.15, 0.7, 0.03], axisbg=axcolor)
-axtc = plt.axes([0.1, 0.2, 0.7, 0.03], axisbg=axcolor)
+axle = plt.axes([0.1, 0.10, 0.7, 0.03], axisbg=axcolor)
+axlo = plt.axes([0.1, 0.05, 0.7, 0.03], axisbg=axcolor)
+axtc = plt.axes([0.1, 0.15, 0.7, 0.03], axisbg=axcolor)
 
-sgamma = Slider(axgamma, r'$\gamma$', gamma_min, gamma_max, valinit=gamma0)
-snu = Slider(axnu, r'$\nu$', nu_min, nu_max, valinit=nu0)
+sle = Slider(axle, r'$\lambda^e$', le_min, le_max, valinit=le0)
+slo = Slider(axlo,r'$\lambda^o$', lo_min,lo_max, valinit=lo0)
 stc = Slider(axtc, r'$T_c$', Tc_min, Tc_max, valinit=Tc0)
-sbeta = Slider(axbeta,r'$\beta$', beta_min,beta_max, valinit=beta0)
 
 def update(val):
-    gamma = sgamma.val
-    nu = snu.val
-    beta = sbeta.val
+    lo = slo.val
+    le = sle.val
     Tc = stc.val
+    alpha = f_alpha(le,lo)
+    beta  = f_beta(le,lo) 
+    gamma = f_gamma(le,lo)
+    nu    = f_nu(le,lo) 
     chi = pyalps.collectXY(data,x='T',y='susceptibility staggered',foreach=['L'])
     plt.sca(axChi)
     for d in chi:
@@ -108,8 +137,22 @@ def update(val):
     plt.cla()
     pyalps.plot.plot(chi)
     plt.xlabel(r'$(T-T_c)L^{\frac{1}{\nu}}$', fontsize='x-large')
-    plt.ylabel(r'$\chi^{\frac{-\gamma}{\nu}}$', fontsize='x-large')
+    plt.ylabel(r'$\chi L^{\frac{-\gamma}{\nu}}$', fontsize='x-large')
     plt.title(r'$\chi$', fontsize='x-large')
+    cv = pyalps.collectXY(data,x='T',y='c_V',foreach=['L'])
+    plt.sca(axCv)
+    for d in cv:
+        d.props['label']='L='+str(d.props['L'])
+        d.x -= Tc
+        d.x = d.x/Tc
+        l = d.props['L']
+        d.x = d.x * pow(float(l),1/nu)
+        d.y = d.y * pow(float(l),-alpha/nu)
+    plt.cla()
+    pyalps.plot.plot(cv)
+    plt.xlabel(r'$(T-T_c)L^{\frac{1}{\nu}}$', fontsize='x-large')
+    plt.ylabel(r'$C_V L^{\frac{-\alpha}{\nu}}$', fontsize='x-large')
+    plt.title(r'$cv$', fontsize='x-large')
     binder = pyalps.collectXY(data,x='T',y='BinderCumulant staggered',foreach=['L'])
     plt.sca(axBinder)
     for d in binder:
@@ -118,7 +161,6 @@ def update(val):
         d.x = d.x/Tc
         l = d.props['L']
         d.x = d.x * pow(float(l),1/nu)
-        #d.y = d.y * pow(float(l),-gamma/nu)
     plt.cla()
     pyalps.plot.plot(binder)
     plt.xlabel(r'$(T-T_c)L^{\frac{1}{\nu}}$', fontsize='x-large')
@@ -136,12 +178,11 @@ def update(val):
     plt.cla()
     pyalps.plot.plot(M)
     plt.xlabel(r'$(T-T_c)L^{\frac{1}{\nu}}$', fontsize='x-large')
-    plt.ylabel(r'$ML^{\frac{-\gamma}{\nu}}$', fontsize='x-large')
+    plt.ylabel(r'$ML^{\frac{\beta}{\nu}}$', fontsize='x-large')
     plt.title(r'$M$', fontsize='x-large')
     fig.canvas.draw_idle()
 
-sgamma.on_changed(update)
-snu.on_changed(update)
+sle.on_changed(update)
 stc.on_changed(update)
-sbeta.on_changed(update)
+slo.on_changed(update)
 plt.show()
